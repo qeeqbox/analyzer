242be72da89e0b1fee4af18db099f6f39c6f4d7e
bd249ce4 <52905881+bd249ce4@users.noreply.github.com>
2019-10-17 22:14:20 -0700

    Update qbstrings.py

--- qbanalyzer/intell/qbstrings.py
+++ qbanalyzer/intell/qbstrings.py
@@ -17,6 +17,11 @@ verbose_flag = False
 class QBStrings:
     @progressbar(True,"Starting QBStrings")
     def __init__(self):
+        '''
+        initialize object with the path of refs that contains References.db
+        Connect to References.db sqlite3 database, get english words from nltk.corpus
+        initialize some suspicious words to detect on
+        '''
         self.refs = path.abspath(path.join(path.dirname( __file__ ),"..", 'refs'))
         if not self.refs.endswith(path.sep): self.refs = self.refs+path.sep
         if not path.isdir(self.refs): mkdir(self.refs)
@@ -30,6 +35,12 @@ class QBStrings:
     @verbose(verbose_flag)
     @progressbar(True,"Detecing english strings")
     def checkwithenglish(self,_data):
+        '''
+        Check if words are UnKnown, english, Partly English or Suspicious
+        
+        Args:
+            _data: data dict
+        '''
         _List = {"UnKnown":[],"English":[],"Partly English":[],"Suspicious":[]}
         if len(self.words) > 0:
             for word in self.words:
@@ -79,8 +90,14 @@ class QBStrings:
             return False
 
     @verbose(verbose_flag)
-    @progressbar(True,"Added descriptions to strings")
+    @progressbar(True,"Check for IPS")
     def checkip(self,_data):
+        '''
+        Check if wordsstripped contains ip or not
+        
+        Args:
+            _data: data dict
+        '''
         _List = []
         ip = compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',I)
         x = findall(ip,self.wordsstripped)
@@ -92,8 +109,14 @@ class QBStrings:
         return True
 
     @verbose(verbose_flag)
-    @progressbar(True,"Added descriptions to strings")
+    @progressbar(True,"Check for Emails")
     def checkemail(self,_data):
+        '''
+        Check if wordsstripped contains emails or not
+        
+        Args:
+            _data: data dict
+        '''
         _List = []
         email = compile('(\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)',I)
         x = findall(email,self.wordsstripped)
@@ -105,8 +128,14 @@ class QBStrings:
         return True
 
     @verbose(verbose_flag)
-    @progressbar(True,"Added descriptions to strings")
+    @progressbar(True,"Check for phone numbers")
     def checkphonenumber(self,_data):
+        '''
+        Check if wordsstripped contains phone numbers or not
+        
+        Args:
+            _data: data dict
+        '''
         _List = []
         tel = compile('(\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)',I)
         x = findall(tel,self.wordsstripped)
@@ -118,21 +147,14 @@ class QBStrings:
         return True
 
     @verbose(verbose_flag)
-    @progressbar(True,"Added descriptions to strings")
+    @progressbar(True,"Check for tags")
     def checktags(self,_data):
-        _List = []
-        html = compile('>([^<]*)<\/',I)
-        x = findall(html,self.wordsstripped)
-        if len(x) > 0:
-            for _ in x:
-                _List.append(_)
-        for x in set(_List):
-            _data.append({"Count":_List.count(x),"TAG":x})
-        return True
-
-    @verbose(verbose_flag)
-    @progressbar(True,"Added descriptions to strings")
-    def check__(self,_data):
+        '''
+        Check if wordsstripped contains tags or not
+        
+        Args:
+            _data: data dict
+        '''
         _List = []
         html = compile('>([^<]*)<\/',I)
         x = findall(html,self.wordsstripped)
@@ -146,6 +168,13 @@ class QBStrings:
     @verbose(verbose_flag)
     @progressbar(True,"Added descriptions to strings")
     def adddescription(self,_type,data,keyword):
+        '''
+        Check if word has description in References.db or not
+        
+        Args:
+            _data: data dict
+            keyword: type of description (pretty much the db table)
+        '''
         if len(data) > 0:
             for x in data:
                 try:
