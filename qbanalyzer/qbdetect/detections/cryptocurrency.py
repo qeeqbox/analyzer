e8c03b55349b3e1ae35c9b6832c53cabe90547b3
bd249ce4 <52905881+bd249ce4@users.noreply.github.com>
2019-11-05 00:06:47 -0800

    Update cryptocurrency.py

--- qbanalyzer/qbdetect/detections/cryptocurrency.py
+++ qbanalyzer/qbdetect/detections/cryptocurrency.py
@@ -4,16 +4,16 @@ from ...logger.logger import logstring,verbose,verbose_flag
 from ...mics.qprogressbar import progressbar
 from re import I, compile, finditer
 
-detections = { "cryptocurrency strings" : [r"Bitcoin|Litecoin|Namecoin|Terracoin|PPcoin|Primecoin|Feathercoin|Novacoin|Freicoin|Devoin|Franko|Megacoin|Quarkcoin|Worldcoin|Infinitecoin|Ixcoin|Anoncoin|BBQcoin|Digitalcoin|Mincoin|Goldcoin|Yacoin|Zetacoin|Fastcoin|I0coin|Tagcoin|Bytecoin|Florincoin|Phoenixcoin|Luckycoin|Craftcoin|Junkcoin"],
-				"btc:Bitcoin Address":[r"[13][a-km-zA-HJ-NP-Z1-9]{25,34}"],
-				"bch:Bitcoin Cash Address":[r"((bitcoincash|bchreg|bchtest):)?(q|p)[a-z0-9]{41}"],
-				"eth:Ethereum Address":[r"0x[a-fA-F0-9]{40}"],
-				"ltc:Litecoin Address":[r"[LM3][a-km-zA-HJ-NP-Z1-9]{26,33}"],
-				"doge:Dogecoin Address":[r"D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}"],
-				"dash:Dash Address":[r"X[1-9A-HJ-NP-Za-km-z]{33}"],
-				"neo:Neo Address":[r"A[0-9a-zA-Z]{33}"],
-				"xrp:Ripple Address":[r"r[0-9a-zA-Z]{33}"],
-				"xmr:Monero Address":[r"4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}"]}
+detections = { "cryptocurrency strings" : [r"\b(Bitcoin|Litecoin|Namecoin|Terracoin|PPcoin|Primecoin|Feathercoin|Novacoin|Freicoin|Devoin|Franko|Megacoin|Quarkcoin|Worldcoin|Infinitecoin|Ixcoin|Anoncoin|BBQcoin|Digitalcoin|Mincoin|Goldcoin|Yacoin|Zetacoin|Fastcoin|I0coin|Tagcoin|Bytecoin|Florincoin|Phoenixcoin|Luckycoin|Craftcoin|Junkcoin)\b"],
+				"btc:Bitcoin Address":[r"\b[13][a-km-zA-HJ-NP-Z1-9]{25,34}\b"],
+				"bch:Bitcoin Cash Address":[r"\b((bitcoincash|bchreg|bchtest):)?(q|p)[a-z0-9]{41}\b"],
+				"eth:Ethereum Address":[r"\b0x[a-fA-F0-9]{40}\b"],
+				"ltc:Litecoin Address":[r"\b[LM3][a-km-zA-HJ-NP-Z1-9]{26,33}\b"],
+				"doge:Dogecoin Address":[r"\bD{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}\b"],
+				"dash:Dash Address":[r"\bX[1-9A-HJ-NP-Za-km-z]{33}\b"],
+				"neo:Neo Address":[r"\bA[0-9a-zA-Z]{33}\b"],
+				"xrp:Ripple Address":[r"\br[0-9a-zA-Z]{33}\b"],
+				"xmr:Monero Address":[r"\b4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}\b"]}
 
 @progressbar(True,"Check Crypto Currency")
 def startanalyzing(data):
