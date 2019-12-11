80f369a002d046d961dcb0771cdc5f9a92d12b97
bd249ce4 <52905881+bd249ce4@users.noreply.github.com>
2019-12-11 15:24:36 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get update && apt-get install -y python3 python3-pip curl libfuzzy-dev yara libmagic-dev libjansson-dev libssl-dev libffi-dev tesseract-ocr libtesseract-dev libssl-dev swig p7zip-full radare2
+sudo apt-get update && apt-get install -y python3 python3-pip curl libfuzzy-dev yara libmagic-dev libjansson-dev libssl-dev libffi-dev tesseract-ocr libtesseract-dev libssl-dev swig p7zip-full radare2 dmg2img
 sudo pip3 install numpy pyelftools macholib macholib python-magic nltk Pillow jinja2 ssdeep pefile scapy r2pipe pytesseract M2Crypto requests tld tldextract bs4 psutil
 sudo python3 -m nltk.downloader words
 sudo ln -s /usr/local/lib/python3.7/site-packages/usr/local/lib/libyara.so /usr/local/lib/libyara.so
