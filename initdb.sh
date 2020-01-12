0bd39b23f83919d69202daeca36f5452bea4a642
qeeqbox <qeeqbox@gmail.com>
2020-01-12 14:38:24 -0800

    fixed

--- initdb.sh
+++ initdb.sh
@@ -1,8 +1,8 @@
 if which mongo ; then
 	mongo QBWindows --eval "db.dropDatabase()"
 	mongo QBResearches --eval "db.dropDatabase()"
-	mongorestore -d QBWindows /databases/Windows/
-	mongorestore -d QBResearches /databases/Researches/
+	mongorestore -d QBWindows databases/Windows/
+	mongorestore -d QBResearches databases/Researches/
 else
 	echo "Please install mongodb"
 fi
