8c3a47ab04c794da2a297f7953837946df4fa96b
qeeqbox <qeeqbox@gmail.com>
2020-01-29 22:04:08 -0800

    v

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
 json_settings = {
-	"mongo_settings_host_docker":"mongodb",
 	"mongo_settings_local":"mongodb://localhost:27017/",
 	"mongo_settings_docker":"mongodb://mongodb:27017/",
 	"function_timeout":120,
