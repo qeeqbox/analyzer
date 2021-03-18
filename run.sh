a8c3b71c79f28bf16b0912a66d0489baf0db9aba
G <52905881+giga-a@users.noreply.github.com>
2021-03-18 12:29:54 -0700

    Add files via upload

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ echo -e "\nQeeqBox Analyzer v$(jq -r '.version' info) starter script -> https://
 setup_requirements () {
 	sudo apt update -y
 	sudo apt install -y linux-headers-$(uname -r) docker.io jq xdg-utils
-	sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-	sudo chmod +x /usr/local/bin/docker-compose
+	curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	chmod +x /usr/local/bin/docker-compose
 	which docker-compose && echo "Good"
 	which docker && echo "Good"
 }
@@ -23,11 +23,14 @@ xdg-open http://127.0.0.1:8000/login
 }
 
 dev_project () {
-	sudo docker-compose -f docker-compose-dev.yml up --build
+	docker-compose -f docker-compose-dev.yml up --build
 }
 
 stop_containers () {
-	sudo docker stop $(sudo docker ps -aq)
+	docker-compose -f docker-compose-test.yml down -v 2>/dev/null
+	docker-compose -f docker-compose-dev.yml down -v 2>/dev/null
+	docker stop $(docker ps | grep analyzer_ | awk '{print $1}') 2>/dev/null
+	docker kill $(docker ps | grep analyzer_ | awk '{print $1}') 2>/dev/null
 } 
 
 deploy_aws_project () {
@@ -40,6 +43,7 @@ auto_configure () {
 	setup_requirements 
 	dev_project 
 	stop_containers
+	kill %%
 }
 
 if [[ "$1" == "auto_configure" ]]; then
@@ -49,6 +53,8 @@ if [[ "$1" == "auto_configure" ]]; then
 	dev_project 
 	stop_containers
 fi
+
+kill %%
 
 while read -p "`echo -e '\nChoose an option:\n1) Setup requirements (docker, docker-compose)\n9) Run auto configuration\n>> '`"; do
   case $REPLY in
