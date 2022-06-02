eb70ab7684ce8569266bd7487261f61196b87ca8
Giga <52905881+giga-a@users.noreply.github.com>
2022-06-02 16:18:05 -0700

    [auto-b] fixed https://github.com/qeeqbox/analyzer/issues/12

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ echo -e "\nQeeqBox Analyzer v$(jq -r '.version' info) starter script -> https://
 setup_requirements () {
 	sudo apt update -y
 	sudo apt install -y linux-headers-$(uname -r) docker.io jq xdg-utils
-	curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	curl -L "https://github.com/docker/compose/releases/latest/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 	chmod +x /usr/local/bin/docker-compose
 	which docker-compose && echo "Good"
 	which docker && echo "Good"
@@ -20,7 +20,7 @@ xdg-open http://127.0.0.1:8000/login
 }
 
 dev_project () {
-	docker-compose -f docker-compose-dev.yml up --build --remove-orphan
+	docker-compose -f docker-compose-dev.yml up --build
 }
 
 stop_containers () {
